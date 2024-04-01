3145f470930ae19978017389a7719a00bf68f0a9
MiguelBM1 <bm15056@ues.edu.sv>
2024-04-01 12:20:41 -0600

    querys busqueda categoria implementados

--- src/main/java/com/app/ecom/web/ProductoGraphQLController.java
+++ src/main/java/com/app/ecom/web/ProductoGraphQLController.java
@@ -5,8 +5,10 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import java.util.List;
-import com.app.ecom.entities.Producto;
 
+import com.app.ecom.entities.Categoria;
+import com.app.ecom.entities.Producto;
+import com.app.ecom.repository.CategoriaRepository;
 import com.app.ecom.repository.ProductoRepository;
 
 @Controller
@@ -15,6 +17,9 @@ public class ProductoGraphQLController {
     @Autowired
     private ProductoRepository productoRepository;
     
+    @Autowired 
+    private CategoriaRepository categoriaRepository;
+
     @QueryMapping
     public List<Producto> listarProductos(){
         return productoRepository.findAll();
@@ -24,9 +29,17 @@ public class ProductoGraphQLController {
     @QueryMapping
     public Producto buscarProductoById(@Argument String id){
         return productoRepository.findById(id).orElseThrow(
-            () -> new RuntimeException("Producto no encontrado"));
+            () -> new RuntimeException("Producto "+ id + " no encontrado"));
     }
 
+    @QueryMapping
+    public List<Categoria> listarCategorias(){
+        return categoriaRepository.findAll();
+    }
 
-
+    @QueryMapping
+    public Categoria buscarCategoriaById(@Argument Long id){
+        return categoriaRepository.findById(id).orElseThrow(
+            () -> new RuntimeException("Categoria "+ id + " no encontrada"));
+    }
 }
