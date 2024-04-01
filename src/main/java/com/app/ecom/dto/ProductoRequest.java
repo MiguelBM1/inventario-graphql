e432588bb9469b7fb5e8498e681ef1643c11de1f
MiguelBM1 <bm15056@ues.edu.sv>
2024-04-01 13:17:06 -0600

    Crud Productos Implementado

---
+++ src/main/java/com/app/ecom/dto/ProductoRequest.java
@@ -0,0 +1,12 @@
+package com.app.ecom.dto;
+
+public record ProductoRequest(
+
+    String id,
+    String nombre,
+    double precio,
+    int cantidad,
+    Long categoriaId
+) {
+
+}
--- src/main/java/com/app/ecom/web/ProductoGraphQLController.java
+++ src/main/java/com/app/ecom/web/ProductoGraphQLController.java
@@ -2,10 +2,13 @@ package com.app.ecom.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import java.util.List;
+import java.util.UUID;
 
+import com.app.ecom.dto.ProductoRequest;
 import com.app.ecom.entities.Categoria;
 import com.app.ecom.entities.Producto;
 import com.app.ecom.repository.CategoriaRepository;
@@ -42,4 +45,42 @@ public class ProductoGraphQLController {
         return categoriaRepository.findById(id).orElseThrow(
             () -> new RuntimeException("Categoria "+ id + " no encontrada"));
     }
+
+    @MutationMapping
+    public Producto guardarProducto(@Argument ProductoRequest productoRequest){
+        Categoria categoria = categoriaRepository.findById(productoRequest.categoriaId()).orElseThrow(
+            () -> new RuntimeException("Categoria "+ productoRequest.categoriaId() + " no encontrada"));
+   
+            Producto productoBBDD = new Producto();
+            productoBBDD.setId(UUID.randomUUID().toString());
+            productoBBDD.setNombre(productoRequest.nombre());
+            productoBBDD.setPrecio(productoRequest.precio());            
+            productoBBDD.setCantidad(productoRequest.cantidad());
+            productoBBDD.setCategoria(categoria);
+            return productoRepository.save(productoBBDD);
+       
+        }
+
+
+        @MutationMapping
+        public Producto actualizarProducto(@Argument String id, @Argument ProductoRequest productoRequest){
+            Categoria categoria = categoriaRepository.findById(productoRequest.categoriaId()).orElseThrow(
+                () -> new RuntimeException("Categoria "+ productoRequest.categoriaId() + " no encontrada"));
+       
+                Producto productoBBDD = new Producto();
+                productoBBDD.setId(id);
+                productoBBDD.setNombre(productoRequest.nombre());
+                productoBBDD.setPrecio(productoRequest.precio());            
+                productoBBDD.setCantidad(productoRequest.cantidad());
+                productoBBDD.setCategoria(categoria);
+                return productoRepository.save(productoBBDD);
+           
+            }
+
+
+            @MutationMapping
+            public void eliminarProducto(@Argument String id){
+                productoRepository.deleteById(id);
+            }
+    
 }
